198737e1d9300272a78983afbe1b6f394805a31e
Fede Lacabaratz <lacarq@gmail.com>
2020-05-21 19:08:05 +0200

    add last updates and finished the project

---
+++ CursoJavaScript/seccion20-proyecto-mostrarEventos-con-eventBriteAPI/eventos/js/app.js
@@ -0,0 +1,32 @@
+// Instanciamos ambas clases
+const eventbrite = new EventBrite();
+const ui = new Interfaz();
+
+// Listener al buscador
+document.getElementById('buscarBtn').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // Leer el texto del input buscar
+    const textoBuscador = document.getElementById('evento').value;
+
+    const categorias = document.getElementById('listado-categorias');
+    const categoriaSeleccionada = categorias.options[categorias.selectedIndex].value;
+
+    // Revisar que haya algo escrito en el buscador
+    if(textoBuscador !== '') {
+        // Mostrar contenido cuando si hay mensaje
+        eventbrite.obtenerEventos(textoBuscador, categoriaSeleccionada)
+            .then(eventos => {
+                if(eventos.eventos.events.length > 0) {
+                    // Si hay eventos
+                    ui.mostrarEventos(eventos.eventos);
+                } else {
+                    // No hay eventos enviar una alerta
+                    ui.mostrarMensaje('No hay eventos para esta búsqueda', 'alert alert-danger mt-4');
+                }
+            })
+    } else {
+        // Mostrar mensaje para que imprima algo
+        ui.mostrarMensaje('Escribe algo en el buscador', 'alert alert-danger mt-4');
+    }
+})
---
+++ CursoJavaScript/seccion20-proyecto-mostrarEventos-con-eventBriteAPI/eventos/js/eventbrite.js
@@ -0,0 +1,33 @@
+class EventBrite {
+    constructor() {
+        this.token_auth = 'ND2WR6ZBWUEVYZESO47D';
+        this.ordenar = 'date';
+    }
+
+    // Obtener los eventos de la REST API
+    async obtenerEventos(evento, categoria) {
+        const respuestaEvento = await fetch(`https://www.eventbriteapi.com/v3/events/search/?=q${evento}&sort_by=${this.ordenar}&categories=${categoria}&token=${this.token_auth}`);
+
+        // Esperar la respuesta del evento y devolverlo como JSON
+        const eventos = await respuestaEvento.json();
+        return {
+            eventos
+        }
+    }
+
+    // Obtiene las categorias en init()
+    async obtenerCategorias() {
+        // Consultar las categorias a la REST API de event brite
+        let url = `https://www.eventbriteapi.com/v3/categories/?token=`;
+
+        const respuestaCategorias = await fetch(`${url}${this.token_auth}`)
+
+        // Esperar la respuesta de las categorias y devolver una respuesta
+        const categorias = await respuestaCategorias.json();
+
+        // devolvemos el resultado
+        return {
+            categorias
+        }
+    }
+}
