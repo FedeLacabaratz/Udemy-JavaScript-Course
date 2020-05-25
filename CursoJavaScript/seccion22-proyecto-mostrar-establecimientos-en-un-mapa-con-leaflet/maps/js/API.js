6fbad2f7b49dc800d53520d8321bba00485ee132
Fede Lacabaratz <lacarq@gmail.com>
2020-05-25 14:18:47 +0200

    add new folder

---
+++ CursoJavaScript/seccion22-proyecto-mostrar-establecimientos-en-un-mapa-con-leaflet/maps/js/API.js
@@ -0,0 +1,21 @@
+class UI {
+    constructor() {
+
+         // Iniciar el mapa
+         this.mapa = this.inicializarMapa();
+
+    }
+
+    inicializarMapa() {
+         // Inicializar y obtener la propiedad del mapa
+         const map = L.map('mapa').setView([19.390519, -99.3739778], 6);
+         const enlaceMapa = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+         L.tileLayer(
+             'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+             attribution: '&copy; ' + enlaceMapa + ' Contributors',
+             maxZoom: 18,
+             }).addTo(map);
+         return map;
+
+    }
+}
