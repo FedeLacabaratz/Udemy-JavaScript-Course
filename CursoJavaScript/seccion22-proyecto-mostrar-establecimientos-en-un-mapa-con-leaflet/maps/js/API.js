3ceb505aee47429adcc6125da22da6e5bf747fdb
Fede Lacabaratz <lacarq@gmail.com>
2020-05-25 18:51:38 +0200

    add updates to exercise

--- CursoJavaScript/seccion22-proyecto-mostrar-establecimientos-en-un-mapa-con-leaflet/maps/js/API.js
+++ CursoJavaScript/seccion22-proyecto-mostrar-establecimientos-en-un-mapa-con-leaflet/maps/js/API.js
@@ -4,22 +4,23 @@ class API {
 
         // Obtener datos desde la API
         const datos = await fetch(`https://www.mapabase.es/arcgis/rest/services/Otros/Gasolineras/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=json`);
-        
+
         // Retornar datos como JSON
         const respuestaJSON = await datos.json();
-        
+
         const respuesta = {
             respuestaJSON
         }
 
         const total = await respuesta.respuestaJSON.features;
-            for(let i=total.length -1; i > 0; i--) {
-                let indexAleatorio = Math.floor(Math.random()*(i+1));
-                let temporal = total[i];
-                total[i] = total[indexAleatorio];
-                total[indexAleatorio] = temporal;
-            }
+        for (let i = total.length - 1; i > 0; i--) {
+            let indexAleatorio = Math.floor(Math.random() * (i + 1));
+            let temporal = total[i];
+            total[i] = total[indexAleatorio];
+            total[indexAleatorio] = temporal;
+        }
         const newTotal = await total.slice(0, 99);
-        
+
         return newTotal
     }
+}
