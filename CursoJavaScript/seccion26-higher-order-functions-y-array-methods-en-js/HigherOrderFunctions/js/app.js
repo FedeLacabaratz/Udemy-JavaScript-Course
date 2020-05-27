9019cb1c8fa62af805de16de78b0f9953d65d25b
Fede Lacabaratz <lacarq@gmail.com>
2020-05-27 15:04:25 +0200

    add updates and folders

--- CursoJavaScript/seccion26-higher-order-functions-y-array-methods-en-js/HigherOrderFunctions/js/app.js
+++ CursoJavaScript/seccion26-higher-order-functions-y-array-methods-en-js/HigherOrderFunctions/js/app.js
@@ -8,7 +8,15 @@ const autos = [
 		color: 'Blanco',
 		transmision: 'automatico'
 	},
-	{ marca: 'Audi', modelo: 'A4', year: 2018, precio: 40000, puertas: 4, color: 'Negro', transmision: 'automatico' },
+	{
+		marca: 'Audi',
+		modelo: 'A4',
+		year: 2018,
+		precio: 40000,
+		puertas: 4,
+		color: 'Negro',
+		transmision: 'automatico'
+	},
 	{
 		marca: 'Ford',
 		modelo: 'Mustang',
@@ -18,7 +26,15 @@ const autos = [
 		color: 'Blanco',
 		transmision: 'automatico'
 	},
-	{ marca: 'Audi', modelo: 'A6', year: 2010, precio: 35000, puertas: 4, color: 'Negro', transmision: 'automatico' },
+	{
+		marca: 'Audi',
+		modelo: 'A6',
+		year: 2010,
+		precio: 35000,
+		puertas: 4,
+		color: 'Negro',
+		transmision: 'automatico'
+	},
 	{
 		marca: 'BMW',
 		modelo: 'Serie 5',
@@ -46,7 +62,15 @@ const autos = [
 		color: 'Rojo',
 		transmision: 'manual'
 	},
-	{ marca: 'Ford', modelo: 'Mustang', year: 2019, precio: 80000, puertas: 2, color: 'Rojo', transmision: 'manual' },
+	{
+		marca: 'Ford',
+		modelo: 'Mustang',
+		year: 2019,
+		precio: 80000,
+		puertas: 2,
+		color: 'Rojo',
+		transmision: 'manual'
+	},
 	{
 		marca: 'Dodge',
 		modelo: 'Challenger',
@@ -56,7 +80,15 @@ const autos = [
 		color: 'Blanco',
 		transmision: 'automatico'
 	},
-	{ marca: 'Audi', modelo: 'A3', year: 2017, precio: 55000, puertas: 2, color: 'Negro', transmision: 'manual' },
+	{
+		marca: 'Audi',
+		modelo: 'A3',
+		year: 2017,
+		precio: 55000,
+		puertas: 2,
+		color: 'Negro',
+		transmision: 'manual'
+	},
 	{
 		marca: 'Dodge',
 		modelo: 'Challenger',
@@ -84,7 +116,15 @@ const autos = [
 		color: 'Blanco',
 		transmision: 'automatico'
 	},
-	{ marca: 'Ford', modelo: 'Mustang', year: 2017, precio: 60000, puertas: 2, color: 'Negro', transmision: 'manual' },
+	{
+		marca: 'Ford',
+		modelo: 'Mustang',
+		year: 2017,
+		recio: 60000,
+		puertas: 2,
+		color: 'Negro',
+		transmision: 'manual'
+	},
 	{
 		marca: 'Dodge',
 		modelo: 'Challenger',
@@ -121,19 +161,95 @@ const autos = [
 		color: 'Blanco',
 		transmision: 'automatico'
 	},
-	{ marca: 'Audi', modelo: 'A4', year: 2016, precio: 30000, puertas: 4, color: 'Azul', transmision: 'automatico' }
+	{
+		marca: 'Audi',
+		modelo: 'A4',
+		year: 2016,
+		precio: 30000,
+		puertas: 4,
+		color: 'Azul',
+		transmision: 'automatico'
+	}
 ];
 
+/* 
+// Antes se utilizaba el "for" para todo
+
+for(let i=0; i<autos.length; i++) {
+	if(autos[i].puertas === 4) {
+		console.log(autos[i])
+	}
+};
+*/
+
+/*
 // forEach
+autos.forEach(auto => {
+	const { marca, modelo, year, precio, puertas, color, transmision } = auto;
+	console.log(auto);
+	
+	//console.log(marca);
+	//console.log(modelo);
+	//console.log(year);
+	//console.log(precio);
+	//console.log(puertas);
+	//console.log(color);
+	//console.log(transmision);
+	
+});
+*/
 
-// map
+/*
+// map (La mayor y única diferencia con el forEach es que map permite crear un nuevo Array y guardar el retorno de valores en ese mismo nuevo Array)
+let resultado = autos.map(auto => {
+	return auto;
+});
+console.log(resultado);
+*/
 
-// filter
+/*
+// filter (Crea un nuevo arreglo basado en una prueba que es evaluada)
+let resultado = autos.filter(auto => {
+	const { marca, color, year, precio, transmision } = auto;
+	//return marca !== 'BMW';
+	//return color === 'Rojo';
+	//return year === 2018;
+	//return year > 2015;
+	//return precio > 50000;
+	//return puertas === 2;
+	//return transmision === 'automatico';
+	//return year >= 2015 && year <= 2017;
+	return year >= 2015 && year <= 2017 && marca === 'BMW';
+});
+console.log(resultado);
+*/
 
-// find
+/*
+// find (Solo retornara el PRIMER elemento que encuentre bajo la condicion a evaluar)
+let resultado = autos.find(auto => {
+	const { marca, modelo }= auto;
+	//return marca === 'BMW';
+	return modelo === 'Mustang';
+});
+console.log(resultado)
+*/
 
-// reduce
+/*
+// reduce (Toma todos los valores y devuelve un valor único)
+//let resultado = autos.reduce((total, auto) => total + auto.precio, 0);
 
-// some
+const numeros = [1,2,3];
+let resultado = numeros.reduce((total, numero) => total + numero, 0)
+
+console.log(resultado);
+*/
+
+
+// some (Evalúa una condicion y retorna "true" o "false" según se cumpla o no)
+//let resultado = autos.some(auto => auto.marca === 'BMW');
+//let resultado = autos.some(auto => auto.marca === 'Ferrari');
+let resultado = autos.some(auto => auto.precio > 1000000);
+
+console.log(resultado)
 
 
